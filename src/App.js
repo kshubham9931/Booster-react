b73df2a1eb2eeedba96fb11b30b5d9afa491a412
Kumar Shubham <73545432+kshubham9931@users.noreply.github.com>
2021-04-28 11:33:57 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const api = `https://randomuser.me/api`;
+  const [user, setUser] = useState([]);
+  const addUserHandler = async () => {
+    const userData = await fetch(api, {
+      method: "GET"
+    });
+    const userJson = await userData.json();
+    //    console.log(userJson.results[0]);
+    const newUser = [...user, userJson.results[0]];
+    setUser(newUser);
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="main-container">
+      <h1>DOM Array Methods in React</h1>
+      {/* <div className="header-btns">
+        <button onClick={addUserHandler}>Add User</button>
+        <input type="text" placeholder="search" />
+        <button id="sort-dsc">sort Desc</button>
+        <button id="sort-asc">sort Asc</button>
+      </div> */}
+      <UserBtn addUserHandler={addUserHandler} />
+      <UserList user={user} />
     </div>
   );
 }
+
+const UserList = (props) => {
+  const { user } = props;
+  return (
+    <div className="user-list">
+      {user.map((userObj, index) => {
+        return <UserObject key={index} userobj={userObj} />;
+      })}
+    </div>
+  );
+};
+
+const UserBtn = ({ addUserHandler }) => {
+  //const { addUserHandler = () => {} } = props;
+  return (
+    <div className="header-btns">
+      <button onClick={addUserHandler}>Add User</button>
+      <input type="text" placeholder="search" />
+      <button id="sort-dsc">sort Desc</button>
+      <button id="sort-asc">sort Asc</button>
+    </div>
+  );
+};
+
+const UserObject = (props) => {
+  let { userobj } = props;
+  return (
+    <div>
+      <div className="header-data">
+        <div className="data-name">
+          {userobj.name.title} {userobj.name.first} {userobj.name.last}
+        </div>
+        <ol>
+          <li>{userobj.email} </li>
+          <li>{userobj.gender} </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
